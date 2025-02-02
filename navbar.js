6f583ac99894131952c6f45a4f3da4e8a123d771
Farron <jones.f03@mymail.sxu.edu>
2025-02-02 14:35:17 -0600

    Small changes to the page.

--- navbar.js
+++ navbar.js
@@ -1,12 +1,18 @@
-document.addEventListener("DOMContentLoaded",function() {
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("navbar").innerHTML = `
         <nav>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.asp">About</a></li>
-                <li><a href="play.html">Play</a></li>
-                <li><a href="community.html">Community</a></li>
-            </ul>
+            <div class="nav-container">
+                <div class="auth-section">
+                    <a href="login.html" class="auth-link">Login / Signup</a>
+                    <img src="./Images/profile-placeholder.png" alt="Profile Picture" class="profile-pic">
+                </div>
+                <ul class="nav-links">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="play.html">Play</a></li>
+                    <li><a href="community.html">Community</a></li>
+                </ul>
+            </div>
         </nav>
     `;
 });
